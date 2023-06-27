7a832bff0603d67975b2a77dcb18b4085327d5a5
unknown <maaher1145@gmail.com>
2023-06-27 21:42:04 +0600

    Made some changes to the header and footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  scrollToTop() {
+    (function scroll() {
+      const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
 
-  date = new Date();
-
-  constructor() { }
-
-  ngOnInit(): void {
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(scroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
+    })();
   }
-
 }
