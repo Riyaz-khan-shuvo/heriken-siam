d654bedf3785bb1c67f2ee593ea913b991b2e0a4
siampathan005@gmail.com <siampathan005@gmail.com>
2023-06-28 17:07:13 +0600

    complete card section

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SubiconComponent } from './subicon/subicon.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [HomeComponent, SubiconComponent],
+  declarations: [HomeComponent, SubiconComponent, ProductsComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
