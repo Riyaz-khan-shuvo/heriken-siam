831f6e836fbe5df48446efe9d75e7135384f91d4
unknown <maaher1145@gmail.com>
2023-07-19 13:43:58 +0600

    Added category dropdown menu to navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   showBox: boolean = false;
-
-  ngOnInit(): void {}
+  showMenu: boolean = false;
 
   toggleSearchBox() {
     this.showBox = !this.showBox;
+  }
+
+  toggleDropdown() {
+    this.showMenu = !this.showMenu;
   }
 }
