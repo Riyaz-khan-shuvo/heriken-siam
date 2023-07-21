896643e1d25c415b1619dbefd959ca0f960f2fa3
unknown <maaher1145@gmail.com>
2023-07-21 11:53:10 +0600

    Made signup form with reactive forms

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  signupForm = this.fb.group({
+    fullName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(128),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(11),
+    ]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
