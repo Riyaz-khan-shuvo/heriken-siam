d7762ab3fb339b10906eb7817835ec547ba20e0d
unknown <maaher1145@gmail.com>
2023-08-06 03:44:12 +0600

    Tried implementing total price and quantity

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,14 +8,36 @@ import { Route } from '@angular/router';
 })
 export class PagesComponent implements OnInit {
   cartProducts: any;
+  productQuantity: number = 1;
+  productPrice: number;
+  totalPrice: number;
 
   constructor() {}
 
   ngOnInit(): void {
     this.getCartProducts();
+    this.getTotalPrice();
   }
 
   getCartProducts() {
     this.cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
+  }
+
+  increaseQuantity() {
+    this.productQuantity = this.productQuantity + 1;
+    return this.productQuantity;
+  }
+
+  decreaseQuantity() {
+    this.productQuantity = this.productQuantity - 1;
+    return this.productQuantity;
+  }
+
+  getTotalPrice() {
+    this.totalPrice = this.productPrice * this.productQuantity;
+    console.log(this.productPrice);
+    console.log(this.productQuantity);
+    console.log(this.totalPrice);
+    return this.totalPrice;
   }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,16 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PagesComponent],
-  imports: [CommonModule, PagesRoutingModule, SharedModule, MaterialModule],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    SharedModule,
+    MaterialModule,
+    FormsModule,
+  ],
 })
 export class PagesModule {}
